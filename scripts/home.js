f6603a24f516d660a47d16fc2a4f95fe9f3ed20a
Jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2025-03-08 12:57:29 -0500

    Store

--- scripts/home.js
+++ scripts/home.js
@@ -2,3 +2,11 @@ document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
 let oLastModif = new Date(document.lastModified);
 let text = document.lastModified; document.getElementById("demo").innerHTML = text;
 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+
+hambutton.addEventListener('click', () => {
+  mainnav.classList.toggle('show');
+  hambutton.classList.toggle('show');
+});
