6d2128b4a25a037e3a40236785f3d82f6e6b56ab
Jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2025-03-22 05:50:10 -0500

    Cralan ddc

--- chamber/scripts/alter.js
+++ chamber/scripts/alter.js
@@ -1,15 +1,3 @@
-document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
-let oLastModif = new Date(document.lastModified);
-let text = document.lastModified; document.getElementById("demo").innerHTML = text;
-
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
-
-
-hambutton.addEventListener('click', () => {
-  mainnav.classList.toggle('show');
-  hambutton.classList.toggle('show');
-});
 
 
 const url = 'data/members.json'; 
