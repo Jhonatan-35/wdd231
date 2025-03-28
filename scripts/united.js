9adbe7fe3f75b737fc40d815132c252c4eeddfba
Jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2025-03-28 13:40:56 -0500

    perseverance

---
+++ scripts/united.js
@@ -0,0 +1,26 @@
+function openModal(id) {
+    let modal = document.getElementById(id);
+    if (modal) {
+        modal.classList.add("show");
+    }
+}
+
+function closeModal(id) {
+    let modal = document.getElementById(id);
+    if (modal) {
+        modal.classList.remove("show");
+    }
+}
+
+
+window.onclick = function (event) {
+    document.querySelectorAll(".modal").forEach((modal) => {
+      if (event.target === modal) {
+        modal.classList.remove("show");
+      }
+    });
+  };
+  document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("timestamp").value = new Date().toISOString();
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+  });
