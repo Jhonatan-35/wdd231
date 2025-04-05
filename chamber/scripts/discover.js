47d909c3d9ea671c98ec068b939dfb9077f8ed17
Jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2025-04-05 02:04:52 -0500

    alsdmfv

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -87,32 +87,5 @@ window.addEventListener("resize", () => {
     }
 });
 
-localStorage.setItem('lastVisit', '2023-02-22');
 
-displayDaysSinceLastVisit();
-
-function displayDaysSinceLastVisit() {
-  const visitsDisplay = document.querySelector('.daysSinceLastVisit');
-
-  const lastVisit = localStorage.getItem('lastVisit');
-
-  if (!lastVisit) {
-    visitsDisplay.innerText = 'This is your first visit';
-    
-    return;
-  }
-
-  const lastVisitDate = Date.parse(lastVisit);
-  
-  if (!lastVisitDate) {
-   
-    return;
-  }
-
-  const currentDate = new Date();
-
-  const difference = currentDate - lastVisitDate;
-  const differenceInDays = Math.floor(difference / (1000 * 60 * 60 * 24));
-
-}
 
