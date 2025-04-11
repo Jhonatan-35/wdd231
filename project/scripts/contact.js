63666cc93a4e472df605379ef9be59539e8cb449
Jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2025-04-11 13:24:48 -0500

    adding

---
+++ project/scripts/contact.js
@@ -0,0 +1,38 @@
+document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
+let oLastModif = new Date(document.lastModified);
+let text = document.lastModified; document.getElementById("demo").innerHTML = text;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+
+hambutton.addEventListener('click', () => {
+  mainnav.classList.toggle('show');
+  hambutton.classList.toggle('show');
+});
+function openModal(id) {
+    let modal = document.getElementById(id);
+    if (modal) {
+        modal.classList.add("show");
+    }
+}
+
+function closeModal(id) {
+    let modal = document.getElementById(id);
+    if (modal) {
+        modal.classList.remove("show");
+    }
+}
+
+
+window.onclick = function (event) {
+    document.querySelectorAll(".marvel").forEach((modal) => {
+      if (event.target === modal) {
+        modal.classList.remove("show");
+      }
+    });
+  };
+  document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("timestamp").value = new Date().toISOString();
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+  });
